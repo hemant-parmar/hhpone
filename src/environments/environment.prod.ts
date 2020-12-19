2795a430b4d923e79d939a3847b34ae7ac25c311
hemant-parmar <hemantparmarh@gmail.com>
2020-12-20 00:55:09 +0530

    update apiUrl of heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://hhpmean2-env.eba-texbppn6.ap-southeast-1.elasticbeanstalk.com/api'
+  apiUrl: 'https://hhpone.herokuapp.com/api'
 };
